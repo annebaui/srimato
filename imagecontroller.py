519fc68f18de7c0bd03fe3add61adae8cfb60246
Anne Bauermeister <info@anne-bauermeister.de>
2012-07-05 00:01:22 +0200

    added functionality
    - metadata is being extracted and displayed as imagefragement with
    common namespace
    - person can be annotated
    - more comments

--- imagecontroller.py
+++ imagecontroller.py
@@ -7,9 +7,24 @@ from models import Image, Imageuri, ImageFragment, Metadata, Annotation
 
 tempimgname = 'tempimg.jpg'
 maximagesize = 680
+defaultfragsize = 10
+gl_image = None
+tempfrag = None
+tempmeta = None
+
+# namespace for W3C Ontology for Media Resources
+ns_ma = 'http://www.w3.org/ns/ma-ont#'
 
 class Imagecontroller(object):
+
     def create_new_image(self, imguri):
+        global gl_image
+        
+        """ Creates a new image instance and queries
+            metadata thats already included in the file
+            resulting in new fragments, metadata and annotations
+        """ 
+        
         imageob = Image.create(width=self.get_image_size(imguri)[0], height=self.get_image_size(imguri)[1],)
         imageob.save()
         imageuri = Imageuri.create(
@@ -17,15 +32,234 @@ class Imagecontroller(object):
             uri=imguri,
         )
         imageuri.save()
+        
+        gl_image = imageob
+        self.extract_def_metadata(imguri)
+        
         return imageob
+    
+    def extract_def_metadata(self, uri):
+        global gl_image
+        global tempmeta
+        global ns_ma
+        
+        """ Extracts selected metadata properties
+            that are already included in a jpg file
+            following the guidelines from MWG
+            and W3C Ontology for Media Resources
+            displayed by the W3C Ontology for Media Resources 
+        """ 
+        
+        tempfrag = self.add_fragment(gl_image, [0,0])
+        tempmeta = self.add_metadata(tempfrag, ns_ma)
+        
+        self.get_img_locator(uri)
+        self.get_img_title(uri)
+        self.get_img_desc(uri)
+        self.get_img_date(uri)
+        self.get_img_creator(uri)
+        self.get_img_copyright(uri)
+        self.get_img_location(uri)
+        self.get_img_policy(uri)
+        self.get_img_relation(uri)
+        self.get_img_relation(uri)
+        
+    def get_img_locator(self, uri):       
+    
+        """ getting image locator info
+            which gets referenced by the uri, from which 
+            the image has been created
+        """ 
+        
+        key = "identifier"
+        self.add_annotation(tempmeta, key, uri)
+    
+    def get_img_title(self, uri):   
 
-    def extract_metadata(self, uri):
+        """ getting image title info
+            which has no MWG guidelines - therefor, the mapping rules from
+            W3C Ontology for Media Resources are used first looking at 
+            XMP Tags and than MWG
+        """ 
+    
+        tags = ["XMP-dc:Title", "MWG:ImageDescription"]
+        key = "title"
+        value = self.get_meta_prop(tags, uri, key)
+        
+    def get_img_desc(self, uri):    
+    
+        """ getting image description info
+            which has no MWG guidelines - therefor, the mapping rules from
+            W3C Ontology for Media Resources are used first looking at 
+            EXIF Tag, than XMP and IPTC
+        """ 
+        
+        tags = ["EXIF:XPSubject", "XMP-dc:Subject", "IPTC:Caption-Abstract"]
+        key = "description"
+        self.get_meta_prop(tags, uri, key)
+    
+    def get_img_date(self, uri):
+    
+        """ getting image date info
+            which following MWG guidelines
+        """ 
+    
+        tags = ["MWG:DateTimeOriginal"]
+        key = "date"
+        self.get_meta_prop(tags, uri, key)
+        
+    def get_img_creator(self, uri):
+    
+        """ getting image creator info
+            which following MWG guidelines
+        """ 
+    
+        tags = ["MWG:Creator"]
+        key = "creator"
+        self.get_meta_prop(tags, uri, key)
+        
+    def get_img_copyright(self, uri):
+    
+        """ getting image copyright info
+            which following MWG guidelines
+        """ 
+        
+        tags = ["MWG:Copyright"]
+        key = "copyright"
+        self.get_meta_prop(tags, uri, key)
+        
+    def get_img_location(self, uri):
+        
+        """ getting image location info
+            which following MWG guidelines
+        """ 
+    
+        tags = ["MWG:Location"]
+        key = "location"
+        self.get_meta_prop(tags, uri, key)
+    
+    def get_img_policy(self, uri):
+        
+        """ getting image policy info
+            which has no MWG guidelines - therefor, the mapping rules from
+            W3C Ontology for Media Resources are used first looking at 
+            XMP-rights:Certificate, than XMP-rights:UsageTerms and XMP-rights:WebStatement
+        """ 
+        
+        tags = ["XMP-rights:Certificate", "XMP-rights:UsageTerms", "XMP-rights:WebStatement"]
+        key = "policy"
+        self.get_meta_prop(tags, uri, key)
+    
+    def get_img_relation(self, uri):
+    
+        """ getting image relation info
+            which has no MWG guidelines - therefor, the mapping rules from
+            W3C Ontology for Media Resources looking at 
+            XMP-dc:Relation
+        """ 
+        
+        tags = ["XMP-dc:Relation"]
+        key = "relation"
+        self.get_meta_prop(tags, uri, key)
+    
+    def get_meta_prop(self, tags, uri, key):
+        
+        """ function for getting any kind of tag of exiftool
+            therefore, an image file is streamed to get a 
+            local copy of the image to work with
+        """
+    
+        global tempmeta
+        
         self.create_temp_img_from_uri(uri)
         with exiftool.ExifTool() as et:
-            metadata = et.get_metadata( tempimgname )
-            print metadata
+            for tag in tags:
+                if et.get_tag(tag, tempimgname) != None:     
+                    self.add_annotation(tempmeta, key, et.get_tag(tag, tempimgname))
+                    return
+        
+    def add_fragment(self, image, coordinates):
+    
+        """ Add a fragment to an image instance requiring
+            coordinates with first value as x and second value as y
+        """ 
+        
+        imgfrag = ImageFragment.create(
+                                image = image,
+                                x = coordinates[0],
+                                y = coordinates[1],
+                                w = defaultfragsize,
+                                h = defaultfragsize,)
+        imgfrag.save()
+        return imgfrag
+        
+    def add_metadata(self, fragment, *nasp):
+    
+        """ Adds metadata to a given fragment 
+            with an optional namespace attribute 
+        """ 
 
+        try:
+            imgmeta = Metadata.get(imgfrag=fragment, namespace=nasp)
+            newimgmetaversion = imgmeta.version
+            newimgmetaversion = newimgmetaversion + 1
+            up_query = Metadata.update(version=newimgmetaversion).where(imgfrag=fragment, namespace=nasp)
+            up_query.execute()
+            return imgmeta
+        except Metadata.DoesNotExist:
+
+            imgmeta = Metadata.create(
+                                imgfrag = fragment,
+                                version = 1,
+                                namespace = nasp,)
+            imgmeta.save()
+            return imgmeta
+        
+    def add_annotation(self, metadata, key, value):
+    
+        """ Adds an Annotation to a Metadata Object
+            of one namespace
+        """ 
+        
+        imganno = Annotation.create(
+                            imgmeta = metadata,
+                            an_key = key,
+                            an_value = value,)
+        imganno.save()
+        return imganno
+    
+    def add_imageuri(self, uri, image):
+        
+        """ 
+            Adds an Imageuri object to a Image Object
+        """ 
+    
+        global ns_ma
+        try:
+            imageuri = Imageuri.get(uri=uri)
+            if imageuri.image == image:
+                return False       
+        except Imageuri.DoesNotExist:
+            imageuri = Imageuri.create(
+                image=image,
+                uri=uri,
+            )
+            imageuri.save()
+            try: 
+                imagefrag = ImageFragment.get(x=0, y=0, image=image)
+                urimeta = self.add_metadata(imagefrag, ns_ma)
+                self.add_annotation(urimeta, "relation", uri)
+                return True
+            except ImageFragment.DoesNotExist:
+                return False
+            return True
+              
     def create_temp_img_from_uri(self, uri): 
+        
+        """ 
+            Create a stream of an image file to work with
+        """
+        
         fp = open(tempimgname, 'wb')
         req = urllib.urlopen(uri)
         for line in req:
@@ -33,11 +267,24 @@ class Imagecontroller(object):
         fp.close()
         
     def get_image_size(self, uri):
+    
+        """ 
+            Gets a current size of an image.
+        """
+            
         fp = urllib.urlopen(uri)
         im = PILImage.open(StringIO.StringIO(fp.read()))
         return im.size
         
     def check_image_size(self, uri):
+        
+        """ 
+            Checks the image size. In the GUI, image
+            has one restriction for display suppose:
+            width < 680px . otherwise the image is being
+            rejected.
+        """
+    
         fp = urllib.urlopen(uri)
         im = PILImage.open(StringIO.StringIO(fp.read()))
         if im.size[0]<=maximagesize:
--- views.py
+++ views.py
@@ -9,6 +9,9 @@ from models import Image, Imageuri, ImageFragment, Metadata, Annotation
 import exiftool
 from imagecontroller import Imagecontroller
 
+imgc = Imagecontroller()
+ns_foaf = 'http://xmlns.com/foaf/0.1/'
+
 @app.route('/')
 def index():
     return image_timeline()
@@ -24,17 +27,15 @@ def image_detail(imageid):
     return render_template('image_detail.html', img=image)
 
 @app.route('/images/<imageid>/addfragment', methods=['GET', 'POST'])
-def addfragment(imageid):
+def addfragment(imageid, *context):
     image = get_object_or_404(Image, id=imageid)
-    defaultfragsize = 10
+
     if request.method == 'POST' and request.form['xvalue']:
-        imgfrag = ImageFragment.create(
-                                image = image,
-                                x = request.form['xvalue'],
-                                y = request.form['yvalue'],
-                                w = defaultfragsize,
-                                h = defaultfragsize,)
-        imgfrag.save()
+        imgfrag = imgc.add_fragment(image, [request.form['xvalue'], request.form['yvalue']])
+        if request.args.get('context', 'person'):
+            fragmeta = imgc.add_metadata(imgfrag, ns_foaf)
+            imgc.add_annotation(fragmeta, 'firstName', request.form['firstName'])
+            imgc.add_annotation(fragmeta, 'lastName', request.form['lastName'])
         flash('Das Fragment wurde erfolgreich angelegt!')
         return redirect(url_for('image_detail', imageid=imageid))
     return render_template('image_detail.html', img=image)
@@ -43,18 +44,11 @@ def addfragment(imageid):
 def adduri(imageid):
     image = get_object_or_404(Image, id=imageid)
     if request.method == 'POST' and request.form['imguri']:
-        try:
-            imageuri = Imageuri.get(uri=request.form['imguri'])
-            if imageuri.image == image:
-                flash('Diese URI existiert bereits fuer dieses Bild - bitte waehle eine andere.', 'error')
-                return redirect(url_for('image_detail', imageid=imageid))
-        except Imageuri.DoesNotExist:
-            imageuri = Imageuri.create(
-                image=image,
-                uri=request.form['imguri'],
-            )
-            imageuri.save()
+        if imgc.add_imageuri(request.form['imguri'], image) == True:
             flash('Die URI wurde erfolgreich hinzugefuegt.')
+            return redirect(url_for('image_detail', imageid=imageid))
+        else: 
+            flash('Diese URI existiert bereits fuer dieses Bild - bitte waehle eine andere.', 'error')
             return redirect(url_for('image_detail', imageid=imageid))
     return render_template('image_detail.html', img=image)
     
@@ -64,7 +58,6 @@ def adduri(imageid):
 # ---
 @app.route('/addimage/', methods=['GET', 'POST'])
 def addimage():
-    maximagesize = 680
     if request.method == 'POST' and request.form['imageuri']:
         try:
             imageuri = Imageuri.get(uri=request.form['imageuri'])
@@ -72,17 +65,8 @@ def addimage():
             flash('Dieses Bild ist bereits im System gespeichert. Bitte waehle eine andere URI.', 'error')
             return redirect(url_for('image_detail', imageid=imageob.id))
         except Imageuri.DoesNotExist:
-            imgc = Imagecontroller()
             if imgc.check_image_size(request.form['imageuri']) == True:
-                imgc.extract_metadata(request.form['imageuri'])
-                """imageob = Image.create(width=200, height=200,)
-                imageob.save()
-                imageuri = Imageuri.create(
-                    image=imageob,
-                    uri=request.form['imageuri'],
-                )
-                imageuri.save()"""
-                imageob = imgc.create_new_image(request.form['imageuri'])
+                imageob = imgc.create_new_image(request.form['imageuri'])                
                 flash('Dein Bild wurde erfolgreich ans System uebertragen. Starte nun mit deinen Annotationen.')
                 return redirect(url_for('image_detail', imageid=imageob.id))
             else:
